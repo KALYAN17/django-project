f789c3028766c1511772261824fe3ed9e4e4c01c
KALYAN17 <gundepudi.kalyan17@gmail.com>
2018-05-04 23:32:00 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+import { UserService } from "src/app/user.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers:[UserService]
+})
+export class AppComponent implements OnInit {
+    userregister;
+    constructor(private userService:UserService) {
+
+    }
+    ngOnInit() {
+        this.userregister = {
+            username: '',
+            password: '',
+            repeatpassword: '',
+            email: '',
+
+        };
+    }
+    register() {
+        this.userService.register(this.userregister).subscribe(
+            response => {
+                alert("User" + this.userregister.username + "registered sucessfully!")
+            },
+            error => console.log("error in registration", error)
+        );
+    }
+  
+}
